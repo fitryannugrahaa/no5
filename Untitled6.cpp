fde3fa22336bb826b9ea27cf91995724d6377f48
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2017-11-08 10:49:22 +0800

    no5

--- Untitled6.cpp
+++ Untitled6.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main () {
-	
+	float berat;
+ int tinggi;
+ 
+ cout << "Masukkan tinggi badan dalam cm : ";
+ cin >> tinggi;
+ 
+ cout << "Masukkan berat badan dalam kg : ";
+ cin >> berat; 
+ 
+ if (berat<tinggi/2.5) {
+  cout << "Hasil : Kurus " << endl;
+ }
+ else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
+  cout <<  "Hasil : Normal" << endl; 
+ }
+ 
+ else if (tinggi/2.3 < berat)
+  cout << "Hasil : Gemuk" << endl;
 }
